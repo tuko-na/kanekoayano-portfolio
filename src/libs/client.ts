4d921472d6f3b484f07f5e93965d2b251a0f6fed
tuko_na <ths30955@ths.hal.ac.jp>
2025-06-09 20:33:28 +0900

    first commit

---
+++ src/libs/client.ts
@@ -0,0 +1,15 @@
+import { createClient } from "microcms-js-sdk";
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+// APIクライアントの初期化
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
